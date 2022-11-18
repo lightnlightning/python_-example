21fd8098b5f4cec7607ab72a27d1c7a23255dab7
lightnlightning <lightning004@outlook.com>
2022-11-18 18:13:31 +0800

    使用全局变量(global)

---
+++ global_test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+ 
+Money = 2000
+def AddMoney():
+   # 想改正代码就取消以下注释:
+   # global Money
+   global Money
+   Money = Money + 1
+ 
+print (Money)
+AddMoney()
+print (Money)
