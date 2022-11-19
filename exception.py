1c602097bdbbcdc2c798cf480666924bb9990d24
lightnlightning <lightning004@outlook.com>
2022-11-19 21:36:15 +0800

    无论是否发生异常都执行finally

--- exception.py
+++ exception.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-# 定义函数
-def temp_convert(var):
-    try:
-        return int(var)
-    except ValueError as Argument:
-        print("参数没有包含数字\n", Argument) 
+try:
+    fh = open("testfile", "w")
+    fh.write("这是一个测试文件，用于测试异常!!\n")
+    print ("这是一个测试文件，用于测试异常!!\n")
+except IOError:
+    print ("Error: 没有找到文件或读取文件失败")
+finally:
+    print ("关闭文件")
+    fh.close()
 
-# 调用函数
-temp_convert("xyz")
+#为了触发异常,禁用testfile文件的写权限
+#在终端执行以下命令
+#chmod -w testfile
+#还原写入权限
+#chmod +w testfile
 
