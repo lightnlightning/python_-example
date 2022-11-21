ee597432464dc3deaa9afbf732a060fa7e8c25ba
lightnlightning <lightning004@outlook.com>
2022-11-21 16:34:58 +0800

    保存异常信息 as

--- exception.py
+++ exception.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-try:
-    fh = open("testfile", "w")
-    fh.write("这是一个测试文件，用于测试异常!!")
-except IOError:
-    print("Error: 没有找到文件或读取文件失败")
-else:
-    print("内容写入文件成功")
-    fh.close()
+# 定义函数
+def temp_convert(var):
+    try:
+        return int(var)
+    except ValueError as Argument:
+        print("参数没有包含数字\n", Argument) 
 
-#为了触发异常,禁用testfile文件的写权限
-#在终端执行以下命令
-#chmod -w testfile
-#还原写入权限
-#chmod +w testfile
+# 调用函数
+temp_convert("xyz")
 
