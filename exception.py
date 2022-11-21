3e40f04bbac95ef4a297b561a31e75b59f934f5e
lightnlightning <lightning004@outlook.com>
2022-11-21 20:53:53 +0800

    自定义抛出异常类class

--- exception.py
+++ exception.py
@@ -1,20 +1,12 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
 
-def foo(s):
-    n = int(s)
-    if n==0:
-        raise ValueError('invalid value: %s' % s)
-    return 10 / n
-
-def bar():
-    try:
-        foo('0')
-    except ValueError as e:
-        print('ValueError!')
-        print(e)
-        # 可以选择把当前的异常又抛出bar(),执行以下代码raise
-        #raise
-
-bar()
-
+#创建了一个类，基类为RuntimeError
+class Networkerror(RuntimeError):
+    def __init__(self, arg):
+        self.args = arg
+try:
+    n = Networkerror("Bad hostname")
+    raise n
+except Networkerror as e:
+    print(e.args) 
