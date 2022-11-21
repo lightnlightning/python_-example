a2011919de29ffd5f7fb9cd4854b0fc96bf5a3be
lightnlightning <lightning004@outlook.com>
2022-11-21 23:59:13 +0800

    析构函数__del__()在对象销毁的时候被调用

--- class.py
+++ class.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
- 
-class Employee:
-   '所有员工的基类'
-   empCount = 0
- 
-   def __init__(self, name, salary):
-      self.name = name
-      self.salary = salary
-      Employee.empCount += 1
-   
-   def displayCount(self):
-     print("Total Employee %d" % Employee.empCount) 
- 
-   def displayEmployee(self):
-      print ("Name : ", self.name,  ", Salary: ", self.salary)
 
-print( "Employee.__doc__:", Employee.__doc__)
-print( "Employee.__name__:", Employee.__name__)
-print( "Employee.__module__:", Employee.__module__)
-print( "Employee.__bases__:", Employee.__bases__)
-print( "Employee.__dict__:", Employee.__dict__)
+class Point:
+   def __init__( self, x=0, y=0):
+      self.x = x
+      self.y = y
+   def __del__(self):
+      class_name = self.__class__.__name__
+      print (class_name, "销毁")
+ 
+pt1 = Point()
+pt2 = pt1
+pt3 = pt1
+
+print (id(pt1), id(pt2), id(pt3)) # 打印对象的id
+del pt1
+del pt2
+del pt3
