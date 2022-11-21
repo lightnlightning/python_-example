a82c476e223162ee51787240c1a3f1655b1e85de
lightnlightning <lightning004@outlook.com>
2022-11-21 23:43:32 +0800

    更改类对象属性

--- class.py
+++ class.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
-
-#类的方法与普通的函数只有一个特别的区别
-#必须有一个额外的第一个参数名称, 按照惯例它的名称是 self。
-class Test1:
-    def prt(self):
-        print(self)
-#self 代表的是类的实例，代表当前对象的地址，
-#self.__class__ 则指向类
-        print(self.__class__)
-
-t1 = Test1()
-t1.prt()
-
-#self 不是 python 关键字，我们把他换成 runoob 也是可以
-class Test2:
-    def prt(runoob):
-        print(runoob)
-        print(runoob.__class__)
  
-t2 = Test2()
-t2.prt()
+class Employee:
+   '所有员工的基类'
+   empCount = 0
+ 
+   def __init__(self, name, salary):
+      self.name = name
+      self.salary = salary
+      Employee.empCount += 1
+   
+   def displayCount(self):
+     print("Total Employee %d" % Employee.empCount) 
+ 
+   def displayEmployee(self):
+      print ("Name : ", self.name,  ", Salary: ", self.salary)
+
+"创建 Employee 类的第一个对象"
+emp1 = Employee("Zara", 2000)
+"创建 Employee 类的第二个对象"
+emp2 = Employee("Manni", 5000)
+emp1.displayEmployee()
+emp2.displayEmployee()
+print ("Total Employee %d" % Employee.empCount)
+
+emp1.age = 7  # 添加一个 'age' 属性
+print(emp1.age)
+emp1.age = 8  # 修改 'age' 属性
+print(emp1.age)
+del emp1.age  # 删除 'age' 属性''''''
+
+
+#你也可以使用以下函数的方式来访问属性：
+
+emp1.age = 7  # 添加一个 'age' 属性
+print(hasattr(emp1, 'age'))    # 如果存在 'age' 属性返回 True。
+print(getattr(emp1, 'age'))    # 返回 'age' 属性的值
+setattr(emp1, 'age', 8) # 添加属性 'age' 值为 8
+delattr(emp1, 'age')    # 删除属性 'age'
+
