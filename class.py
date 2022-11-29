155067cc770daa4ddb675a7a6a4fe6315a4a353c
lightnlightning <lightning004@outlook.com>
2022-11-29 10:43:03 +0800

    类私有变量

--- class.py
+++ class.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
 # -*- coding: UTF-8 -*-
  
-class Vector:
-   def __init__(self, a, b):
-      self.a = a
-      self.b = b
+
+class JustCounter:
+    __secretCount = 0  # 私有变量
+    publicCount = 0    # 公开变量
  
-   def __str__(self):
-      return 'Vector (%d, %d)' % (self.a, self.b)
-   
-   def __add__(self,other):
-      return Vector(self.a + other.a, self.b + other.b)
- 
-v1 = Vector(2,10)
-v2 = Vector(5,-2)
-print (v1 + v2)
+    def count(self):
+        self.__secretCount += 1
+        self.publicCount += 1
+        print (self.__secretCount) 
+
+counter = JustCounter()
+counter.count()
+counter.count()
+print (counter.publicCount)
+print (counter._JustCounter__secretCount)  # object._className__attrName访问私有变量 
+print (counter.__secretCount)  # 报错，实例不能访问私有变量
+
